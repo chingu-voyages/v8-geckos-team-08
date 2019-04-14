6d0cde6000d5a7f63c7a7af11149f70cc5b4f3f1
Abigail Swarth <abigailswarth@gmail.com>
2019-04-14 16:13:22 -0400

    boilerplate setup for chrome extension

---
+++ build/precache-manifest.6fdb672920ee4ad6ded6f574539a7c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a22c103e98551196d087",
+    "url": "/static/js/main.0216e594.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "a22c103e98551196d087",
+    "url": "/static/css/main.90c55af0.chunk.css"
+  },
+  {
+    "revision": "71c0ab20c24ceafd384f346a7136488e",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App practice-styles">
+        <div className="header">Reminder App</div>
+      </div>
+    );
+  }
+}
+
+export default App;
