0b60a02f93da437827256ea3fdf96999790d4b7c
janetmndez <janetmendez.03@gmail.com>
2019-05-04 23:32:15 -0400

    added styles to timer tiles

--- src/components/TimerTileComponent.js
+++ src/components/TimerTileComponent.js
@@ -6,19 +6,19 @@ export default class TimerTileComponent extends Component {
 		this.countdown(this.props.time * 60);
 	}
 
-	 countdown = (seconds) => {
-		 let timerId, timerTitle;
-		 if (this.props) {
+	countdown = (seconds) => {
+		let timerId, timerTitle;
+		if (this.props) {
 			timerId = this.props.id;
 			timerTitle = this.props.title;
 		 }
-		seconds = parseInt(sessionStorage.getItem(`${timerId}-seconds`))||seconds;
+		seconds = parseInt(sessionStorage.getItem(`${timerId}-seconds`)) || seconds;
 
 		function tick() {
 			seconds--;
 			sessionStorage.setItem(`${timerId}-seconds`, seconds)
 			let counter = document.getElementById(`${timerId}-timer`);
-			let current_minutes = parseInt(seconds/60);
+			let current_minutes = parseInt(seconds / 60);
 			let current_seconds = seconds % 60;
 			if (counter) {
 				counter.innerHTML = current_minutes + ":" + (current_seconds < 10 ? "0" : "") + current_seconds;
@@ -36,17 +36,14 @@ export default class TimerTileComponent extends Component {
 
 		return (
 			<div className='timer-tile-container'>
-				<div className='title'>
-					{title}
-				</div>
-				<div className='type'>
-					{type}
-				</div>
-				<div className='time'>
-					{time}
+				<div className='timer-information'>
+					<div className='title'>{title}</div>
+					<div className='details'>
+						{type} / {time}
+					</div>
 				</div>
 				<div className='timer-countdown'>
-				<div id={`${id}-timer`}>{time}:00</div>
+					<div id={`${id}-timer`}>{time}:00</div>
 				</div>
 			</div>
 		)
